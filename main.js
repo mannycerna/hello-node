b7c4234da5a82568a25cd318b8e049e81fbcc79f
mannycerna <manuel.cerna@codeimmersives.com>
2022-09-12 13:31:07 -0500

    Create new main.js file.

---
+++ main.js
@@ -0,0 +1,7 @@
+console.log ("Hello World!")
+
+console.log ("lll  lll                                    lll      dd !!!!")
+console.log ("lll  lll  oooo  ww       ww   oooo  rr rr   lll      dd !!!!")
+console.log ("lll  lll oo  oo ww       ww  oo  oo rrr  r  lll  dddddd ")
+console.log ("lll  lll oo  oo  ww  ww  ww  oo  oo rr      lll dd   dd !!!!")
+console.log ("lll  lll  oooo    ww    ww    oooo  rr      lll  dddddd !!!!")
